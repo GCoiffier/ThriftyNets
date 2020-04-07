2ee3a6c33a56170133754ba40d9678e8cfe6cfa2
GCoiffier <guillaume.coiffier.42@orange.fr>
2020-04-07 13:54:46 +0200

    e

--- train_imagenet.py
+++ train_imagenet.py
@@ -141,13 +141,11 @@ parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
 
-'''
-parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+parser.add_argument('-a', '--arch', metavar='ARCH', default='ResThrifty',
                     choices=model_names,
                     help='model architecture: ' +
                         ' | '.join(model_names) +
                         ' (default: resnet18)')
-'''
 
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
