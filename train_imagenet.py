4495d4259901833bda9adc71b8340cf39cf1cd39
GCoiffier <guillaume.coiffier.42@orange.fr>
2020-04-08 14:20:26 +0200

    imagenet

--- train_imagenet.py
+++ train_imagenet.py
@@ -36,8 +36,13 @@ class ResNetEmbedder(nn.Module):
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        """
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        """
+        
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -68,17 +73,27 @@ class ResNetEmbedder(nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
+
         x = self.layer1(x)
         x = self.layer2(x)
-        x = self.layer3(x)
         return x
+        """
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+        """
 
 class ResThriftyNet(nn.Module):
 
     def __init__(self, n_filters, n_iter, n_history, pool_strategy, activ="relu", conv_mode="classic", bias=False):
         super(ResThriftyNet, self).__init__()
         
-        self.Lembed = ResNetEmbedder(resnet.BasicBlock, [3, 4, 6])
+        self.Lembed = ResNetEmbedder(resnet.BasicBlock, [3, 4])
 
         self.input_shape = (32,28,28) # output shape of the embedder
 
