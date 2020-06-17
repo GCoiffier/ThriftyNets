e8f43256393e68f97fa61ad11bc11364c7ba979c
GCoiffier <guillaume.coiffier.42@orange.fr>
2020-06-17 13:49:46 +0200

    reduceLR monitor only cross entropy

--- common/losses.py
+++ common/losses.py
@@ -3,6 +3,8 @@ import torch.nn.functional as F
 
 class LossFun:
 
+    name = "NULL"
+
     def __init__(self):
         pass
 
@@ -11,11 +13,17 @@ class LossFun:
 
 
 class CrossEntropy(LossFun):
+    
+    name = "CrossEntropy"
+
     def call(self, output, target, trainer):
         return F.cross_entropy(output, target)
 
 
 class AlphaLoss(LossFun):
+    
+    name = "AlphaLoss"
+
     def call(self, output, target, trainer):
         temp = trainer.temperature
         x = trainer.model.Lblock.alpha.data
