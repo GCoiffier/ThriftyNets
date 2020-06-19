59d236c7d9596bb296fe7a6b169a19ae1f19c9da
GCoiffier <guillaume.coiffier.42@orange.fr>
2020-06-19 14:06:21 +0200

    verbose scheduler

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     scheduler = None
     if args.optimizer=="sgd":
         optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
-        scheduler = ReduceLROnPlateau(optimizer, factor=args.gamma, patience=args.patience, min_lr=args.min_lr)
+        scheduler = ReduceLROnPlateau(optimizer, factor=args.gamma, patience=args.patience, min_lr=args.min_lr, verbose=True)
     elif args.optimizer=="adam":
         optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
 
