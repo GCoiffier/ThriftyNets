d3894a9623db142b57737dc3f1dbf73ff8c96ee9
GCoiffier <guillaume.coiffier.42@orange.fr>
2020-06-24 15:26:53 +0200

    new benches

---
+++ bench_T10.sh
@@ -0,0 +1,6 @@
+python3 train.py --n-params 40000 -T 10 -pool 2 -H 1 --name hist1_10T_1 --epochs 150 --steps 50 100 130
+python3 train.py --n-params 40000 -T 10 -pool 2 -H 1 --name hist1_10T_2 --epochs 150 --steps 50 100 130
+python3 train.py --n-params 40000 -T 10 -pool 2 -H 1 --name hist1_10T_3 --epochs 150 --steps 50 100 130
+python3 train.py --n-params 40000 -T 10 -pool 2 -H 5 --name hist5_10T_1 --epochs 150 --steps 50 100 130
+python3 train.py --n-params 40000 -T 10 -pool 2 -H 5 --name hist5_10T_2 --epochs 150 --steps 50 100 130
+python3 train.py --n-params 40000 -T 10 -pool 2 -H 5 --name hist5_10T_3 --epochs 150 --steps 50 100 130
